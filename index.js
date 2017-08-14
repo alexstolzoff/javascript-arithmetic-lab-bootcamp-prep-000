6af1b9e740df8bbe69d6d1fa6c24f1e0e835f0aa
Alex Stolzoff <alexstolznut@hotmail.com>
2017-08-14 22:12:02 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,18 @@
+function add(a,b){
+  return a+b;
+}
+function divide(a,b){
+  return a/b;
+}
+function subtract(a,b){
+  return a-b;
+}
+function multiply(a,b){
+  return a*b;
+}
+
+
+add(1,1);
+divide(2,2);
+subtract(1,2);
+multiply(2,2);
